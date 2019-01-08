dbb9abf396e05a4c5dc229f417b1298d38ec146f
panteleimonov <panteleimonov@yahoo.com>
2019-01-08 21:07:11 +0200

    01.08

---
+++ app/js/common.js
@@ -0,0 +1,15 @@
+// $(function() {
+
+	
+
+
+// });
+
+window.onload = function() {
+	
+	var heroSkew         = document.querySelector('.hero-skew');
+	var	headervideoFrame = document.querySelector('.header-video iframe');
+
+	heroSkew.classList.add('hero-animation');
+	headervideoFrame.classList.add('loaded');
+}
